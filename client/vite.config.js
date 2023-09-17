aa0c595a4c954bb369dd4559d2abb127ffa2e325
khavinshankar <khavinshankar@gmail.com>
2023-09-17 11:14:54 +0530

    added server url env support

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: process.env.SERVER_URL ?? "http://localhost:4000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
